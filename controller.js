8d19c90d58390fbfed05c2dd30dda079e86fba13
turboflanagan <turboflanagan@gmail.com>
2015-11-16 19:01:56 -0500

    added angular functionality

--- controller.js
+++ controller.js
@@ -1,22 +1,72 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('myController', housesListCrl);
-
-
-	function housesListCrl($scope){
-
-	var index = 1;
-	$scope.houses = [houses[0]]
-
+var myApp = angular.module('houseList', []);
+myApp.controller('housesListCtrl', housesListCtrl);
+myApp.controller('editController', editController);
+	
+function housesListCtrl($scope){
+	var numDisplayedHouses = 0;
+	$scope.houses = [];
 	$scope.addHouse = function(){
-		$scope.houses.push(houses[index++])
+		$scope.houses = houses.slice(0,++numDisplayedHouses);
 	}
-	$scope.enableAdd = function(){
-		return index < houses.length;
-	}
-
-	$scope.removeHouses = function(){
+	$scope.clearHouses = function(){
 		$scope.houses = [];
-		index = 0;
+		numDisplayedHouses = 0;
 	}
-	};
 
+	$scope.enableAdd = function(){
+		return houses.length > numDisplayedHouses
+	}
+}
+
+function editController($scope){
+	$scope.houses = houses;
+	$scope.view = 'list'
+	var viewIndex = -1;
+
+	$scope.changeView = function(view){
+		$scope.view = view;
+	}
+
+
+	$scope.addNewHouse = function(){
+		$scope.houses.push(new House($scope.nickName, $scope.imgSrc, $scope.address, $scope.city, $scope.state, $scope.zipCode, $scope.forSale, $scope.salePrice, $scope.estimatedPrice, $scope.estimatedMortgage, $scope.imgWidth, $scope.discount) )
+		$scope.changeView('list')
+		$scope.nickName = '';
+		$scope.imgSrc = '';
+		$scope.address = '';
+		$scope.city = '';
+		$scope.state = '';
+		$scope.zipCode = '';
+		$scope.forSale = '';
+		$scope.salePrice = '';
+		$scope.estimatedPrice = '';
+		$scope.discount = '';
+		$scope.estimatedMortgage = '';
+	}
+
+	$scope.deleteHouse = function(index){
+		$scope.changeView('delete')
+		viewIndex = index
+		$scope.thisHouse = $scope.houses[index];
+
+	}
+
+	$scope.confirmDelete = function(){
+		$scope.changeView('list');
+		
+		$scope.houses.splice(viewIndex, 1);
+		
+	}
+
+	$scope.editHouse = function(index){
+		$scope.changeView('edit');
+		$scope.nickName = $scope.houses[index].nickName
+		viewIndex = index;
+	}
+	
+	$scope.confirmEdit = function(){
+		$scope.houses[viewIndex].nickName = $scope.nickName
+		$scope.nickName = ''
+		$scope.changeView('list')
+	}
+}
--- homes.js
+++ homes.js
@@ -1,25 +1,21 @@
 
-
-function House(nicName, imgSrc, width, address, city, state, zip, status, price, Zestimate, description, estMortgage) {
-	this.nicName = nicName;
-	this.imgSrc = imgSrc;
-	this.width = width;
-	this.address = address;
-	this.city = city;
-	this.state = state;
-	this.zip = zip;
-	this.status = status;
-	this.price = price;
-	this.Zestimate = Zestimate;
-	this.description = description;
-	this.estMortgage = estMortgage;
-
-
+function House(nickName, imgSrc, address, city, state, zipCode, forSale, salePrice, estimatedPrice, estimatedMortgage, imgWidth, discount){
+		this.nickName = nickName;
+        this.imgSrc = imgSrc;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.forSale = forSale;
+        this.salePrice = salePrice;
+        this.estimatedPrice = estimatedPrice;
+        this.discount = discount;
+        this.estimatedMortgage = estimatedMortgage;
+        this.imgWidth = imgWidth
 }
 
-var houses = [];
-houses.push(new House("The Villa", "http://photos3.zillowstatic.com/p_h/IShf3c5r7s3xw60000000000.jpg", "300", "4747 Northside Dr", "Atlanta", "GA", "30327", "For Sale", "2,999,000",  "4,031,664", "A good deal!! (25% off!)", "11,179/mo"));
-houses.push(new House("The Pool House", "http://photos2.zillowstatic.com/p_h/IS1n6j1pnfqv2b1000000000.jpg", "300", "1101 Garmon Dr NW", "Atlanta", "GA", "30327", "For Sale", "2,399,000", "2,684,66", "Average Deal", "8,943/mo"));
-houses.push(new House("The Awesome", "http://ak.t1.tiles.virtualearth.net/tiles/cmd/ObliqueHybrid?a=03200231131-48125-20-101&g=4510", "200", "914 Davis Dr", "Atlanta", "GA", "30327", "NOT for Sale", "---", "2,134,055", "---", "---"));
-houses.push(new House("Versailles", "http://photos3.zillowstatic.com/p_h/ISx35uo0fixyhb0000000000.jpg", "200", "5115 Northside Dr", "Atlanta", "GA", "30327", "For Sale", "8,000,000", "12,360,014", "A crazy deal!! (30% off!)", "29,821/mo"));
-
+var houses=[];
+houses.push(new House('The Villa', 'http://photos3.zillowstatic.com/p_h/IShf3c5r7s3xw60000000000.jpg', '4747 Nortside Dr.', 'Atlanta', 'GA', '30327', 'For Sale', '2,999,000', '4,031,664', '11,179/mo', '300', (100 - (2999000/4031664) *100).toFixed(2)));
+houses.push(new House('The Pool House', 'http://photos2.zillowstatic.com/p_h/IS1n6j1pnfqv2b1000000000.jpg', '1101 Garmon Dr', 'Atlanta', 'GA', '30327', 'For Sale', '2,399,000', '2,684,662', '8,943/mo', '300', (100 - (2399000/2684662) *100).toFixed(2)));
+houses.push(new House('The Awesome House', 'http://ak.t1.tiles.virtualearth.net/tiles/cmd/ObliqueHybrid?a=03200231131-48125-20-101&g=4510', '914 Davis Dr', 'Atlanta', 'GA', '30327', 'NOT for Sale', '-', '2,134,055', '-', '200', '-'));
+houses.push(new House('Versailles', 'http://photos3.zillowstatic.com/p_h/ISx35uo0fixyhb0000000000.jpg', '5115 Northside Dr', 'Atlanta', 'GA', '30327', 'For Sale', '8,000,000', '12,360,014', '29,821/mo', '200', (100 - (8000000/12360014) *100).toFixed(2)));
